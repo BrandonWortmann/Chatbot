261da8d8b6d51b3dc112e6abcf0516caa5999bad
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2017-10-23 11:58:50 -0600

    I added a picture to the popup

--- src/chat/controller/ChatRunner.java
+++ src/chat/controller/ChatRunner.java
@@ -1,10 +1,19 @@
 package chat.controller;
 
+import chat.view.PopupDisplay;
+
 public class ChatRunner
 {
 	public static void main(String[] args)
 	{
-		ChatController app = new ChatController();
-		app.start();
+		PopupDisplay test = new PopupDisplay();
+		test.displayText("Hi Im a penguin");
+		test.collectResponse("ask a question");
+		
+		
+		
+		
+		///ChatController app = new ChatController();
+		//app.start();
 	}
 }
