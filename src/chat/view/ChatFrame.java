7ad497d078f2563f60965c8c4889c5dd7feaa196
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2017-11-13 13:45:31 -0700

    created the famework for the JFRame

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -3,7 +3,28 @@ package chat.view;
 import chat.controller.ChatbotController;
 import javax.swing.JFrame;
 
-public class ChatFrame
+public class ChatFrame extends JFrame
 {
+	private ChatbotController appController;
+	private ChatPanel appPane;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with the chatbot");
+		this.setResizable(true);
+		this.setSize(600, 600);
+		this.setVisible(true);
+		
+		
+		
+	}
 
 }
---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,9 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.ChatbotController;
+
+public class ChatPanel
+{
+
+}
