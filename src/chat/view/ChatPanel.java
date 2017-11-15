3869379bf452cbd53af12c629abaa6eaf64e6b18
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2017-11-15 12:44:08 -0700

    I created the stubs for the JPanel

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -2,8 +2,41 @@ package chat.view;
 
 import javax.swing.JPanel;
 import chat.controller.ChatbotController;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+import java.awt.Color;
 
-public class ChatPanel
+
+
+public class ChatPanel extends JPanel
 {
+	private ChatbotController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
+	
+	public ChatPanel(ChatbotController appController)
+	{
+		
+	}
 
+	private void setupPanel()
+	{
+		
+	}
+
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupLiseners()
+	{
+		
+	}
+	
+	
 }
