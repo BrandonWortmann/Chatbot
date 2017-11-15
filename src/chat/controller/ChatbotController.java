82ed098c3de35b18fa76fce3c6fa81fdf795f5f3
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2017-11-15 12:26:20 -0700

    I added the frame to my controller

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -2,29 +2,32 @@ package chat.controller;
 
 import chat.model.Chatbot;
 import chat.view.PopupDisplay;
+import chat.view.ChatFrame;
 
 public class ChatbotController
 {
 	
 	private Chatbot chatbot;
 	private PopupDisplay display;
+	private ChatFrame appFrame;
 	
 	
 	public ChatbotController()
 	{
 		chatbot = new Chatbot("Brandon Wortmann");
 		display = new PopupDisplay();
+		appFrame = new ChatFrame(this);
 	}
 	
 	public void start()
 	{
 		String response = display.collectResponse("What do you want to talk about");
 		
-		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
-		{
-			response = popupChat(response);
-			response = display.collectResponse(response);
-		}
+//		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
+//		{
+//			response = popupChat(response);
+//			response = display.collectResponse(response);
+//		}
 	}
 	
 	private String popupChat(String chat)
